825a67c30b203b0a0825fa668509c95f016dd5e9
majid-d-d <majidsoltaniantiranchi@gmail.com>
2023-07-01 17:45:55 +0330

    Create InMemoryDataBase.cs

---
+++ TransactionVisualizerTest/InMemoryDataBase.cs
@@ -0,0 +1,13 @@
+﻿namespace TransactionVisualizer.Database;
+
+public class InMemoryDataBase : DBContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TransactionVisualizer");
+    }
+}
