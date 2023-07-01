68710e2adb21f43459d954f9d1c7effb6aae9439
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-07-01 13:20:56 +0330

    add account parser

---
+++ TransactionVisualizer/Utility/Parser.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using CsvHelper;
+
+namespace TransactionVisualizerTest;
+
+public class Parser<T> : IParser<T>
+{
+    public List<T> Pars(string path)
+    {
+        List<T> data = new List<T>();
+        
+        using (var reader = new StreamReader(path))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            data = csv.GetRecords<T>().ToList();
+        }
+        
+        return data;
+    }
+}
---
+++ TransactionVisualizerTest/AccountParserTest.cs
@@ -0,0 +1,42 @@
+using TransactionVisualizer.Models;
+using TransactionVisualizer.Models.ParserModel;
+using Xunit.Abstractions;
+
+namespace TransactionVisualizerTest;
+
+public class AccountParserTest
+{
+    private ITestOutputHelper _out;
+
+    public AccountParserTest(ITestOutputHelper @out)
+    {
+        _out = @out;
+    }
+
+    [Fact]
+    public void ParseAccountTest()
+    {
+        string path =
+            "/Users/mahdimazaheri/Programming/Mohaymen/TransactionVisualizer/TransactionVisualizerTest/Accounts.csv";
+        IParser<FlatAccount> iParser = new Parser<FlatAccount>();
+        FlatAccount flatAccount = new FlatAccount
+        {
+            AccountID = 6534454617,
+            CardID = "6104335000000190",
+            Sheba = "IR120778801496000000198",
+            AccountType = "پس انداز",
+            BranchTelephone = "55638667",
+            BranchAdress = "تهران-خیابان خیام-بالاتر از چهارراه گلوبندک",
+            BranchName = "گلوبندک",
+            OwnerName = "افسر",
+            OwnerFamilyName = "طباطبایی",
+            OwnerID = 1227114110
+        };
+
+
+        FlatAccount flatAccounts = iParser.Pars(path)[0];
+        
+        // _out.WriteLine(accounts);
+        Assert.Equivalent(flatAccounts, flatAccount);
+    }
+}
