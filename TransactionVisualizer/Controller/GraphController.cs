4c3fa0f48190546164dc0e5fc013d43c2beec91f
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-07-09 09:46:00 +0330

    fix graph model

---
+++ TransactionVisualizer/Controller/GraphController.cs
@@ -0,0 +1,106 @@
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using TransactionVisualizer.DataRepository;
+using TransactionVisualizer.Models;
+using TransactionVisualizer.Models.Graph;
+using TransactionVisualizer.Models.ParserModel;
+using TransactionVisualizer.Models.Transaction;
+using TransactionVisualizer.Utility.Converters;
+using TransactionVisualizer.Utility.Graph;
+using TransactionVisualizer.Utility.ParserUtils;
+
+namespace TransactionVisualizer.Controller;
+
+[Route("[controller]/[action]")]
+public class GraphController : Microsoft.AspNetCore.Mvc.Controller
+{
+    private IGraphProcessor<Account, Transaction> _graphProcessor;
+
+    EdgeRepository edgeRepository = new EdgeRepository();
+
+    public GraphController(IGraphProcessor<Account, Transaction> graphProcessor)
+    {
+        _graphProcessor = graphProcessor;
+    }
+
+
+    [HttpGet]
+    public ActionResult<string> Graph()
+    {
+        // Create a custom graph
+        var graph = new CustomGraph<Account, Transaction>();
+
+        EdgeRepository repository = new EdgeRepository();
+        List<Edge<Account, Transaction>> edges = edgeRepository.Search(p => p.MatchAll());
+
+        
+        _graphProcessor._graph = graph;
+        edges.ForEach(item =>
+        {
+            _graphProcessor._graph.AddEdge(item);
+        });
+
+        var sourceAccount = edges[3].Source;
+        var sinkAccount = edges[6].Destination;
+
+        Console.WriteLine($"finding all path for {sourceAccount.AccountID} => {sinkAccount.AccountID}");
+        var path = _graphProcessor.GetAllPaths(sourceAccount, sinkAccount);
+
+        path.ForEach(p =>
+        {
+            p.ForEach(item =>
+                {
+                    Console.Write(item.Source.AccountID + "=>" + item.Destination.AccountID + " " +
+                                  item.EdgeContent.Amount + ",");
+                }
+            );
+            Console.WriteLine();
+        });
+
+        var maxFlow = _graphProcessor.GetMaxFlow(sourceAccount, sinkAccount);
+        Console.WriteLine(
+            "Max flow from " + sourceAccount.AccountID + " to " + sinkAccount.AccountID + " is " + maxFlow);
+
+        return new ActionResult<string>("Max flow from " + sourceAccount.AccountID + " to " + sinkAccount.AccountID +
+                                        " is " + maxFlow);
+    }
+
+
+    [HttpGet]
+    public ActionResult<string> ToElastic()
+    {
+        string accountPath =
+            "/Users/mahdimazaheri/Programming/Mohaymen/TransactionVisualizer/TransactionVisualizerTest/Accounts.csv";
+        string transactionPath =
+            "/Users/mahdimazaheri/Programming/Mohaymen/TransactionVisualizer/TransactionVisualizerTest/Transaction.csv";
+
+        var accountParser = new Parser<FlatAccount>();
+        var transactionParser = new Parser<FlatTransaction>();
+
+        var flatAccounts = accountParser.Pars(accountPath);
+        var flatTransactions = transactionParser.Pars(transactionPath);
+
+        List<Edge<Account, Transaction>> edges = new List<Edge<Account, Transaction>>();
+
+
+        IFlatToFullConverter<Account, FlatAccount> accountFlatToFull = new FlatAccountToAccount();
+        var account = accountFlatToFull.ConvertAll(flatAccounts);
+
+        IFlatToFullConverter<Transaction, FlatTransaction> transactionFlatToFull = new FlatTransactionToTransaction();
+        var transaction = transactionFlatToFull.ConvertAll(flatTransactions);
+        transaction.ForEach(item =>
+        {
+            var edge = new Edge<Account, Transaction>
+            {
+                Source = account.Find(a => a.AccountID == item.SourceAccount),
+                Destination = account.Find(a => a.AccountID == item.DestiantionAccount),
+                EdgeContent = item,
+                weight = item.Amount
+            };
+            edges.Add(edge);
+        });
+
+        edgeRepository.AddAll(edges);
+        return "finish";
+    }
+}
---
+++ TransactionVisualizer/DataRepository/EdgeRepository.cs
@@ -0,0 +1,24 @@
+using Nest;
+using TransactionVisualizer.DataRepository.ElasticRepository;
+using TransactionVisualizer.Models;
+using TransactionVisualizer.Models.Graph;
+using TransactionVisualizer.Models.Transaction;
+
+namespace TransactionVisualizer.DataRepository;
+
+public class EdgeRepository
+{
+    private IDataRepository _dataRepository = new ElasticDataRepository("http://localhost:9200/", "edges");
+
+    public void AddAll(List<Edge<Account, Transaction>> edges)
+    {
+        _dataRepository.InsertAll(edges);
+    }
+
+    public List<Edge<Account, Transaction>> Search(
+        Func<SearchDescriptor<Edge<Account, Transaction>>, ISearchRequest> selector)
+    {
+        var response = _dataRepository.Search(selector);
+        return response.Items!;
+    }
+}
