19e14346c987c588e702927c5025e38dd492b227
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-07-09 12:41:20 +0330

    fix interrupted file

--- TransactionVisualizer/Models/Account/Account.cs
+++ TransactionVisualizer/Models/Account/Account.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
-namespace TransactionVisualizer.Models;
+namespace TransactionVisualizer.Models.Account;
 
 
 public class Account 
--- TransactionVisualizerTest/UtilityTest/Converters/FlatTransactionToTransactionConverterTest.cs
+++ TransactionVisualizerTest/UtilityTest/Converters/FlatTransactionToTransactionConverterTest.cs
@@ -27,9 +27,9 @@ public class FlatTransactionToTransactionConverterTest
 
         // Assert
         result.Should().NotBeNull();
-        result.Id.Should().Be(flatTransaction.TransactionId);
-        result.SourceAccountId.Should().Be(flatTransaction.SourceAccountId);
-        result.DestinationAccountId.Should().Be(flatTransaction.DestinationAccountId);
+        result.ID.Should().Be(flatTransaction.TransactionId);
+        result.SourceAccount.Should().Be(flatTransaction.SourceAccountId);
+        result.DestiantionAccount.Should().Be(flatTransaction.DestinationAccountId);
         result.TransactionType.Should().Be(TransactionType.Paya);
         result.Amount.Should().Be(flatTransaction.Amount);
         result.Date.Should().Be(DateTimeParser.ParseExact(flatTransaction.Date));
@@ -62,9 +62,9 @@ public class FlatTransactionToTransactionConverterTest
 
         foreach (var (flatTransaction, transaction) in flatTransactions.Zip(result, (f, t) => (f, t)))
         {
-            transaction.Id.Should().Be(flatTransaction.TransactionId);
-            transaction.SourceAccountId.Should().Be(flatTransaction.SourceAccountId);
-            transaction.DestinationAccountId.Should().Be(flatTransaction.DestinationAccountId);
+            transaction.ID.Should().Be(flatTransaction.TransactionId);
+            transaction.SourceAccount.Should().Be(flatTransaction.SourceAccountId);
+            transaction.DestiantionAccount.Should().Be(flatTransaction.DestinationAccountId);
             transaction.TransactionType.Should().Be(flatTransaction.Type.ParsTransactionType());
             transaction.Amount.Should().Be(flatTransaction.Amount);
             transaction.Date.Should().Be(DateTimeParser.ParseExact(flatTransaction.Date));
